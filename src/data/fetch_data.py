d038bae056ecea23390c0d82fa9533ab094c93cc
Timon Gehrig <timon.gehrig@bf.uzh.ch>
2023-11-26 17:22:16 +0100

    FF fetched for exact time, added processing data

--- src/data/fetch_data.py
+++ src/data/fetch_data.py
@@ -3,9 +3,14 @@ import pandas as pd
 import requests
 import yfinance as yf
 import os
+from io import StringIO
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 # Define start and end data for data you want to fetch
+# The timeseries will start the first of the month after the specified month. E.g. for 2014-08-01, the first data point will bi on 2014-09-01
+# Note: after the return calculation the first row will be useless. E.g. we choose 2014-08-01 that our data then starts on 2014-10-01
 start_date = '2014-08-01'
 end_date = '2022-08-01'
 
@@ -22,7 +27,7 @@ tickers = constituents.Ticker.to_list()
 
 def fetch_fama_french():
 
-    # URL of the csv file
+    # URL of the CSV file
     csv_url = "http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_Factors.CSV"
     
     # Send a GET request to the URL
@@ -30,11 +35,29 @@ def fetch_fama_french():
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Save the content to a local file
-        with open("F-F_Research_Data_Factors.CSV", "wb") as file:
-            file.write(response.content)
+        # Read CSV starting from row 4 until row 1154
+        df = pd.read_csv(StringIO(response.text), skiprows=3, nrows=1154)
 
-        return "CSV file downloaded successfully."
+        # Convert the 'Date' column to pandas datetime format
+        df['Unnamed: 0'] = pd.to_datetime(df['Unnamed: 0'], format='%Y%m')
+
+        # Convert the start_date string to a datetime object, add to month for reasons specified under the definition of the start date
+        start_date_object = datetime.strptime(start_date, "%Y-%m-%d") + relativedelta(months=2)
+
+        # Format the datetime object to the desired format
+        formatted_start_date = start_date_object.strftime("%Y%m") 
+
+        # Specify the cutoff date
+        cutoff_date = pd.to_datetime(formatted_start_date, format='%Y%m')
+
+        # Filter rows based on the condition
+        df = df[df['Unnamed: 0'] >= cutoff_date]
+
+        # Save the content to a local file
+        file_path = "data/raw/F-F_Research_Data_Factors.CSV"
+        df.to_csv(file_path, index=False)
+
+        return f"CSV file downloaded successfully and saved to: {file_path}"
     else:
         return f"Failed to retrieve the file. Status code: {response.status_code}"
     
@@ -100,6 +123,15 @@ def fetch_esg(tickers):
 
     return combined_esg_data
 
-print(fetch_fama_french())
-print(fetch_monthly_returns(start_date,end_date,tickers))
-print(fetch_esg(tickers))
+
+# Specify the folder path
+folder_path = 'data/raw/'
+
+# Create the full path by joining the folder path and file name
+output_path_mr = f'{folder_path}monthly_returns.csv'
+output_path_esg = f'{folder_path}esg_data.csv'
+
+# Fetch data and save to folder data raw
+fetch_monthly_returns(start_date,end_date,tickers).to_csv(output_path_mr, index=False)
+fetch_esg(tickers).to_csv(output_path_esg, index=False)
+fetch_fama_french()
