4b0f8abeba86a3655a8e89bb7c08323fc5b947a4
mariiakzmn <mariiiakuzmina@gmail.com>
2023-12-04 15:19:34 +0100

    SQL db connection

--- src/models/regression_v1.py
+++ src/models/regression_v1.py
@@ -4,19 +4,31 @@
 ####################################################
 
 import pandas as pd
+from sqlalchemy import create_engine
+
+# Create an SQLAlchemy engine
+engine = create_engine('sqlite:///data/financial_data.db')
+
+# SQL queries to select all data from each table
+esg_query = "SELECT * FROM esg_data"
+monthly_returns_query = "SELECT * FROM monthly_returns"
+
+# Read the data into pandas DataFrames
+esgdata = pd.read_sql(esg_query, engine)
+monthlyreturns = pd.read_sql(monthly_returns_query, engine)
 
 # Import monthly returns
-monthlyreturns = pd.read_csv(r'C:\Users\jenni\Desktop\extern_monthly_returns.csv')
-monthlyreturns = monthlyreturns.drop('Date', axis=1) # Drops date, not needed for regression
+# monthlyreturns = pd.read_csv(r'C:\Users\jenni\Desktop\extern_monthly_returns.csv')
+# monthlyreturns = monthlyreturns.drop('Date', axis=1) # Drops date, not needed for regression
 monthlyreturns = monthlyreturns.drop(['GEHC'], axis=1) # This is a column which only contains NaN
 
 # Import changed ESG data
-esgdata =  pd.read_csv(r'C:\Users\jenni\Desktop\esg_data_change.csv')
-esgdata = esgdata.drop('Date', axis=1)
+# esgdata =  pd.read_csv(r'C:\Users\jenni\Desktop\esg_data_change.csv')
+# esgdata = esgdata.drop('Date', axis=1)
 
 # Mean imputation
-monthlyreturns = monthlyreturns.fillna(monthlyreturns.mean())
-esgdata = esgdata.fillna(esgdata.mean())
+# monthlyreturns = monthlyreturns.fillna(monthlyreturns.mean())
+# esgdata = esgdata.fillna(esgdata.mean())
 
 # Regression specifications
 import statsmodels.formula.api as smf
